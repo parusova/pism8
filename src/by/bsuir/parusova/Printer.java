8119ada7a53664ed528f46d4b0acbea5762dafa6
parusova <anna.parusova@mail.ru>
2017-12-07 12:50:06 +0300

    Added new method.

--- src/by/bsuir/parusova/Printer.java
+++ src/by/bsuir/parusova/Printer.java
@@ -4,4 +4,10 @@ class Printer {
     static void print(String str) {
         System.out.println("Output: " + str);
     }
+	
+	static void printMessages(String[] messages) {
+		for (String message : messages) {
+			System.out.println("Output[" + (i + 1) + "]: " + message);
+		}
+	}
 }
